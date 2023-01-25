4734317e969aae4cc7dd87735ec84767880edb18
Chike Ibezim <ibezimchike@gmail.com>
2023-01-25 11:29:46 +0100

    implementing subscriptions complete

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -4,6 +4,55 @@ import { useQuery, gql } from '@apollo/client';
 
 import Link from './Link';
 
+//define subscription for newlinks
+const NEW_LINKS_DOCUMENT = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy{
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
+
+//define subscription for newVotes
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 //wite the graphql query
 //the FEED_QUERY variable uses gql, a library that uses tagged template literals
@@ -39,8 +88,8 @@ const LinkList = () => {
       the hook returns 3 items:
         i. loading: true as long as the request us still ongoing
         ii. error: In case the request fails: contain info of what went wrong
-        iii. datat: actual data received from the seerver. It has the links property
-        iv. which represents a list of link elements
+        iii. data: actual data received from the seerver. It has the links property
+        which represents a list of link elements
       Note: there are many more items the hook returns but this 3 are the most important
 
     */
@@ -60,9 +109,31 @@ const LinkList = () => {
 
         we can also pass a field called updateQuery which can be use to update the cache.
     */
+
+    //adding subscription for new links
     subscribeToMore({
         document: NEW_LINKS_DOCUMENT,
-        updateQuery
+        updateQuery: (prev, { subscriptionData }) => {
+            if(!subscriptionData.data) return prev;
+            const newLink = subscriptionData.data.newLink;
+            const exists = prev.feed.links.find(
+                ({ id }) => id === newLink.id
+            );
+            if(exists) return prev;
+
+            return Object.assign([], prev, {
+                feed: {
+                    links: [newLink, ...prev.feed.links],
+                    count: prev.feed.links.length + 1,
+                    _typename: prev.feed.__typename
+                }
+            })
+        }
+    });
+
+    //adding subscription for votes
+    subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
     })
 
     return (
